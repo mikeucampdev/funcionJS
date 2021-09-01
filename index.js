7387522d7ce68f73bd4d45f299cdec2778cbe958
Mr Mike <hemontemayor.c02@ucamp.utel.edu.mx>
2021-08-31 20:45:16 -0500

    haciendo una funcion

---
+++ index.js
@@ -0,0 +1,7 @@
+const numero1 = document.getElementById('numero1')
+const numero1 = document.getElementById('numero2')
+
+function sumarDosNumeros(numero1, numero2) {
+    console.log (numero1, numero2)
+}
+
